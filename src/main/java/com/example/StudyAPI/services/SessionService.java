1ceda800a070ab9164765b8859def399c5194cb4
Daniel Carvalho Doria <danielcarvalhodoria@gmail.com>
2026-08-27 19:37:57 -0300

    update

--- src/main/java/com/example/StudyAPI/services/SessionService.java
+++ src/main/java/com/example/StudyAPI/services/SessionService.java
@@ -1,11 +1,16 @@
 package com.example.StudyAPI.services;
 
+import com.example.StudyAPI.dtos.sessionDtos.SessionRequestDto;
+import com.example.StudyAPI.dtos.sessionDtos.SessionResponseDto;
+import com.example.StudyAPI.models.StudySession;
 import com.example.StudyAPI.models.User;
 import com.example.StudyAPI.repositories.SessionsRepository;
 import com.example.StudyAPI.repositories.UserRepository;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class SessionService {
@@ -20,5 +25,47 @@ public class SessionService {
         String email = SecurityContextHolder.getContext().getAuthentication().getName();
         return userRepository.findByEmail(email)
                 .orElseThrow(() -> new IllegalArgumentException("User not found."));
+    }
+
+    public SessionResponseDto convertToDto(StudySession studySession){
+        return new SessionResponseDto(
+                studySession.getId(),
+                studySession.getTitle(),
+                studySession.getSubject(),
+                studySession.getNotes(),
+                studySession.getDate()
+        );
+    }
+
+    public List<SessionResponseDto> list(){
+        User user = getLoggedUser();
+        return repository.findByUser(user)
+                .stream()
+                .map(this::convertToDto)
+                .toList();
+    }
+
+    public SessionResponseDto create(SessionRequestDto dto){
+        User user = getLoggedUser();
+        StudySession session = new StudySession(
+                dto.getTitle(),
+                dto.getSubject(),
+                dto.getDurationMinutes(),
+                dto.getNotes(),
+                dto.getDate()
+        );
+        session.setUser(user);
+        repository.save(session);
+        return convertToDto(session);
+    }
+
+    public SessionResponseDto findById(Long id){
+        User user = getLoggedUser();
+        StudySession session = repository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Session not found"));
+        if (!session.getUser().getId().equals(user.getId())){
+            throw new IllegalArgumentException("Acess denied.")
+        }
+        return convertToDto(session);
     }
 }
